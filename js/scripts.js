b0e9079aa3f1271483428028ea49c360bb97ca3f
ruitaiS <shaoruitai@gmail.com>
2020-05-26 00:48:28 -0400

    copied code for 3-way state switch, added jQuery to scripts; need to do setup / formatting ot make it actually run though

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,14 @@ function say(msg, where){
     document.getElementById(where).innerHTML = msg;
 };
 
+function flip(){
+};
+
+function genGrid (x, y){
+
+
+}
+
 function makeRows(n){
 //generate text that makes n number of rows, each with unique id
 //call fillRow on each of them
@@ -27,3 +35,14 @@ function fillRow(totalCols, rowNum){
     document.getElementById("row"+rowNum).innerHTML = res;
 
 };
+
+//doesn't work lol
+//copied & pasted from https://codyhouse.co/ds/components/info/button-states
+//Learn jQuery when you get the chance, I'm sure the code here works but it's just not formatted properly in some dumb tiny way that's making it break..
+
+$('.gameButton').on('click', function(event){
+    if($(this).hasClass("floor")){ $(this).removeClass("floor").addClass("wall"); return;}
+    else if($(this).hasClass("wall")){ $(this).removeClass("wall").addClass("obj"); return;}
+    else if($(this).hasClass("obj")){ $(this).removeClass("obj").addClass("bot"); return;}
+    else { $(this).removeClass("bot").addClass("floor"); return;}
+});
